705f0e42f548fa5b128488928be3ef42da7c58d8
Lucas Kivi <lukekiv@gmail.com>
2022-02-20 22:47:26 -0600

    Add skeleton for handler and server.

---
+++ proj_dir/java/src/server/ImageProcessingNodeHandler.java
@@ -0,0 +1,18 @@
+/**
+ * Created by:
+ * - Lucas Kivi (kivix019)
+ * - Charles Droeg (droeg022)
+ */
+
+
+import pa1.ImageProcessingNode;
+import pa1.InvalidLocation;
+import pa1.TaskReceipt;
+import pa1.TaskRequest;
+
+
+public class ImageProcessingNodeHandler implements ImageProcessingNode.Iface {
+    public TaskReceipt send(TaskRequest task) throws InvalidLocation, org.apache.thrift.TException {
+
+    }
+}
