fe088c9d7f4e4e00f6e18e6730ea88baf6ee3f7b
Lucas Kivi <lukekivi@gmail.com>
2022-02-23 13:42:38 -0600

    Added a mock node.

--- proj_dir/java/src/node/ImageProcessingNodeHandler.java
+++ proj_dir/java/src/node/ImageProcessingNodeHandler.java
@@ -9,11 +9,18 @@ import pa1.ImageProcessingNode;
 import pa1.InvalidLocation;
 import pa1.TaskReceipt;
 import pa1.TaskRequest;
+import pa1.TaskStatus;
+import pa1.SchedulingPolicy;
 import org.apache.thrift.TException;
 
 
 public class ImageProcessingNodeHandler implements ImageProcessingNode.Iface {
-    public TaskReceipt send(TaskRequest task) throws InvalidLocation, TException {
-            
+    public TaskReceipt sendTask(TaskRequest task) throws InvalidLocation {
+            return new TaskReceipt(
+                task.getTask(),
+                TaskStatus.SUCCESS,
+                SchedulingPolicy.MANDATORY,
+                "Successful task completion."
+            );
     }
 }
