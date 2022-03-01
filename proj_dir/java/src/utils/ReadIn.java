d787a7951dc8cc64e8c7810608ea3fe6234d3e4b
Charles Droege <droegecharlie@gmail.com>
2022-02-28 23:07:18 -0600

    Changed up ReadIn format

--- proj_dir/java/src/utils/ReadIn.java
+++ proj_dir/java/src/utils/ReadIn.java
@@ -23,19 +23,37 @@ public class ReadIn {
         try {
             Scanner scanConfig = new Scanner(file);
             String[] line; // Read in node lines
-            for (int i = 0; i < 4; i++) { // Looping to get addresses of the nodes
+
+            while (scanConfig.hasNextLine()) {
                 line = scanConfig.nextLine().split(" ");
-                if (!line[0].equals("node_" + i)) {
-                    System.out.println("Improper Configuration file.\n");
-                    System.exit(1);
+                if (line[0].equals("node_0")) {
+                    address = line[1]; // address
+                    prob = Double.parseDouble(line[2]); // probability
+                    port = Integer.parseInt(line[3]); // port number
+                    nodes[0] = new NodeData(address, prob, port);
+                } else if (line[0].equals("node_1")) {
+                    address = line[1]; // address
+                    prob = Double.parseDouble(line[2]); // probability
+                    port = Integer.parseInt(line[3]); // port number
+                    nodes[1] = new NodeData(address, prob, port);
+                } else if (line[0].equals("node_2")) {
+                    address = line[1]; // address
+                    prob = Double.parseDouble(line[2]); // probability
+                    port = Integer.parseInt(line[3]); // port number
+                    nodes[2] = new NodeData(address, prob, port);
+                } else if (line[0].equals("node_3")) {
+                    address = line[1]; // address
+                    prob = Double.parseDouble(line[2]); // probability
+                    port = Integer.parseInt(line[3]); // port number
+                    nodes[3] = new NodeData(address, prob, port);
                 }
-                address = line[1]; // address
-                prob = Double.parseDouble(line[2]); // probability
-                port = Integer.parseInt(line[3]); // port number
-                nodes[i] = new NodeData(address, prob, port);
             }
         } catch (Exception e) {
             System.out.println("Improper Configuration file.\n");
+            System.exit(1);
+        }
+        if (nodes[0] == null || nodes[1] == null || nodes[2] == null || nodes[3] == null) {
+            System.out.println("Improper Configuration file - Invalid Nodes given.\n");
             System.exit(1);
         }
         return nodes;
@@ -47,19 +65,19 @@ public class ReadIn {
         try {
             Scanner scanConfig = new Scanner(file);
             String[] line; // Read in client line
-            line = scanConfig.nextLine().split(" "); // Iterating through node lines
-            line = scanConfig.nextLine().split(" ");
-            line = scanConfig.nextLine().split(" ");
-            line = scanConfig.nextLine().split(" ");
 
-            line = scanConfig.nextLine().split(" "); // Client address
-            if (!line[0].equals("server")) {
-                System.out.println("Improper Configuration file.\n");
-                System.exit(1);
+            while (scanConfig.hasNextLine()) {
+                line = scanConfig.nextLine().split(" ");
+                if (line[0].equals("server")) {
+                    ans = line[1];
+                }
             }
-            ans = line[1];
         } catch (Exception e) {
             System.out.println("Improper Configuration file.\n");
+            System.exit(1);
+        }
+        if (ans.equals("")) {
+            System.out.println("Improper Configuration file - No Server given.\n");
             System.exit(1);
         }
         return ans;
@@ -71,20 +89,19 @@ public class ReadIn {
         try {
             Scanner scanConfig = new Scanner(file);
             String[] line; // Read in client line
-            line = scanConfig.nextLine().split(" "); // Iterating through node and server lines
-            line = scanConfig.nextLine().split(" ");
-            line = scanConfig.nextLine().split(" ");
-            line = scanConfig.nextLine().split(" ");
-            line = scanConfig.nextLine().split(" ");
 
-            line = scanConfig.nextLine().split(" "); // Client address
-            if (!line[0].equals("client")) {
-                System.out.println("Improper Configuration file.\n");
-                System.exit(1);
+            while (scanConfig.hasNextLine()) {
+                line = scanConfig.nextLine().split(" "); // Client address
+                if (line[0].equals("client")) {
+                    ans = line[1];
+                }
             }
-            ans = line[1];
         } catch (Exception e) {
             System.out.println("Improper Configuration file.\n");
+            System.exit(1);
+        }
+        if (ans.equals("")) {
+            System.out.println("Improper Configuration file - No Client given.\n");
             System.exit(1);
         }
         return ans;
@@ -96,31 +113,28 @@ public class ReadIn {
         try {
             Scanner scanConfig = new Scanner(file);
             String[] line; // Read in client line
-            line = scanConfig.nextLine().split(" "); // Iterating through node, server, and client lines
-            line = scanConfig.nextLine().split(" ");
-            line = scanConfig.nextLine().split(" ");
-            line = scanConfig.nextLine().split(" ");
-            line = scanConfig.nextLine().split(" ");
-            line = scanConfig.nextLine().split(" ");
 
-            line = scanConfig.nextLine().split(" "); // Scheduling Policy
-            if (!line[0].equals("policy")) {
-                System.out.println("Improper Configuration file.\n");
-                System.exit(1);
-            }
-
-            if (line[1].equals("random")) {
-                policy = SchedulingPolicy.MANDATORY;
-            }
-            else if (line[1].equals("balancing")){
-                policy = SchedulingPolicy.OPTIONAL;
-            }
-            else {
-                System.out.println("Improper Configuration file.\n");
-                System.exit(1);               
+            while (scanConfig.hasNextLine()) {
+                line = scanConfig.nextLine().split(" "); // Scheduling Policy
+                if (line[0].equals("policy")) {
+                    if (line[1].equals("random")) {
+                        policy = SchedulingPolicy.MANDATORY;
+                    }
+                    else if (line[1].equals("balancing")){
+                        policy = SchedulingPolicy.OPTIONAL;
+                    }
+                    else {
+                        System.out.println("Improper Configuration file - Invalid Policy.\n");
+                        System.exit(1);
+                    }
+                }
             }
         } catch (Exception e) {
-            System.out.println("Improper Configuration file.\n");
+            System.out.println("Improper Configuration file\n");
+            System.exit(1);
+        }
+        if (policy == null) {
+            System.out.println("Improper Configuration file - No Policy given.\n");
             System.exit(1);
         }
         return policy;
@@ -132,22 +146,19 @@ public class ReadIn {
         try {
             Scanner scanConfig = new Scanner(file);
             String[] line; // Read in client line
-            line = scanConfig.nextLine().split(" "); // Iterating through node, server, and client lines
-            line = scanConfig.nextLine().split(" ");
-            line = scanConfig.nextLine().split(" ");
-            line = scanConfig.nextLine().split(" ");
-            line = scanConfig.nextLine().split(" ");
-            line = scanConfig.nextLine().split(" ");
-            line = scanConfig.nextLine().split(" ");
 
-            line = scanConfig.nextLine().split(" "); // Scheduling Policy
-            if (!line[0].equals("data")) {
-                System.out.println("Improper Configuration file.\n");
-                System.exit(1);
+            while (scanConfig.hasNextLine()) {
+                line = scanConfig.nextLine().split(" "); // Scheduling Policy
+                if (line[0].equals("data")) {
+                    ans = line[1];
+                }
             }
-            ans = line[1];
         } catch (Exception e) {
             System.out.println("Improper Configuration file.\n");
+            System.exit(1);
+        }
+        if (ans.equals("")) {
+            System.out.println("Improper Configuration file - No Data path given.\n");
             System.exit(1);
         }
         return ans;
@@ -159,15 +170,18 @@ public class ReadIn {
         try {
             Scanner scanConfig = new Scanner(file);
             String[] line; // Read in thrift path line
-
-            line = scanConfig.nextLine().split(" "); // thrift path
-            if (!line[0].equals("THRIFT_LIB_PATH")) {
-                System.out.println("Improper Environment file.\n");
-                System.exit(1);
+            while (scanConfig.hasNextLine()) {
+                line = scanConfig.nextLine().split(" "); // thrift path
+                if (line[0].equals("THRIFT_LIB_PATH")) {
+                    ans = line[1];
+                }
             }
-            ans = line[1];
         } catch (Exception e) {
             System.out.println("Improper Environment file.\n");
+            System.exit(1);
+        }
+        if (ans.equals("")) {
+            System.out.println("Improper Environment file - No Thrift path given.\n");
             System.exit(1);
         }
         return ans;
@@ -179,16 +193,19 @@ public class ReadIn {
         try {
             Scanner scanConfig = new Scanner(file);
             String[] line; // Read in thrift path line
-            line = scanConfig.nextLine().split(" "); // Skip to openCV file line
 
-            line = scanConfig.nextLine().split(" "); // openCV path
-            if (!line[0].equals("OPENCV_LIB_PATH")) {
-                System.out.println("Improper Environment file.\n");
-                System.exit(1);
+            while (scanConfig.hasNextLine()) {
+                line = scanConfig.nextLine().split(" "); // openCV path
+                if (line[0].equals("OPENCV_LIB_PATH")) {
+                    ans = line[1];
+                }
             }
-            ans = line[1];
         } catch (Exception e) {
             System.out.println("Improper Environment file.\n");
+            System.exit(1);
+        }
+        if (ans.equals("")) {
+            System.out.println("Improper Environment file - No OpenCV path given.\n");
             System.exit(1);
         }
         return ans;
@@ -200,17 +217,19 @@ public class ReadIn {
         try {
             Scanner scanConfig = new Scanner(file);
             String[] line; // Read in Proj path line
-            line = scanConfig.nextLine().split(" "); // Iterating until proj line
-            line = scanConfig.nextLine().split(" ");
 
-            line = scanConfig.nextLine().split(" "); // proj path
-            if (!line[0].equals("PROJ_PATH")) {
-                System.out.println("Improper Environment file.\n");
-                System.exit(1);
+            while (scanConfig.hasNextLine()) {
+                line = scanConfig.nextLine().split(" "); // proj path
+                if (line[0].equals("PROJ_PATH")) {
+                    ans = line[1];
+                }
             }
-            ans = line[1];
         } catch (Exception e) {
             System.out.println("Improper Environment file.\n");
+            System.exit(1);
+        }
+        if (ans.equals("")) {
+            System.out.println("Improper Environment file - No Project path given.\n");
             System.exit(1);
         }
         return ans;
@@ -242,7 +261,10 @@ public class ReadIn {
         } catch (Exception exception) {
             System.out.println("ReadIn: getServerPort() - " + exception);
         }
-      
+        if (ans == -1) {
+            System.out.println("Improper Configuration file - Invalid Server port given.\n");
+            System.exit(1);
+        }
         return ans;
     }
 
@@ -250,6 +272,14 @@ public class ReadIn {
     //     ReadIn r = new ReadIn();
     //     NodeData[] nodes;
     //     nodes = r.getNodes("/project/droeg022/Distributed-Image-Processing/proj_dir/machine.txt");
+    //     String serverA = r.getServer("/project/droeg022/Distributed-Image-Processing/proj_dir/machine.txt");
+    //     String clientA = r.getClient("/project/droeg022/Distributed-Image-Processing/proj_dir/machine.txt");
+    //     String dataPath = r.getData("/project/droeg022/Distributed-Image-Processing/proj_dir/machine.txt");
+    //     String thriftPath = r.getThriftPath("/project/droeg022/Distributed-Image-Processing/proj_dir/env.txt");
+    //     String opencvPath = r.getThriftPath("/project/droeg022/Distributed-Image-Processing/proj_dir/env.txt");
+    //     String projPath = r.getThriftPath("/project/droeg022/Distributed-Image-Processing/proj_dir/env.txt");
+    //     int serverPort = r.getServerPort("/project/droeg022/Distributed-Image-Processing/proj_dir/machine.txt");
+
     //     System.out.println("Node 0 address: " + nodes[0].getAddress());
     //     System.out.println("Node 1 address: " + nodes[1].getAddress());
     //     System.out.println("Node 2 address: " + nodes[2].getAddress());
@@ -264,5 +294,13 @@ public class ReadIn {
     //     System.out.println("Node 1 port: " + nodes[1].getPort());
     //     System.out.println("Node 2 port: " + nodes[2].getPort());
     //     System.out.println("Node 3 port: " + nodes[3].getPort());
+
+    //     System.out.println("Server Address: " + serverA);
+    //     System.out.println("Client Address: " + clientA);
+    //     System.out.println("Data Path: " + dataPath);
+    //     System.out.println("Thrift path: " + thriftPath);
+    //     System.out.println("opencv path: " + opencvPath);
+    //     System.out.println("proj_path path: " + projPath);
+    //     System.out.println("Server Port num: " + serverPort);
     // }
 }
