db0d14bfff63ff5d74c001110592168cc42288e0
Lucas Kivi <lukekivi@gmail.com>
2022-02-28 23:09:42 -0600

    Update JavaClient to handle no location of config files.

--- proj_dir/java/src/client/JavaClient.java
+++ proj_dir/java/src/client/JavaClient.java
@@ -9,19 +9,21 @@ import org.apache.thrift.protocol.TBinaryProtocol;
 import org.apache.thrift.protocol.TProtocol;
 import org.apache.thrift.transport.TSocket;
 import org.apache.thrift.transport.TTransport;
-import pa1.*;
+import pa1.InvalidLocation;
+import pa1.JobReceipt;
+import pa1.JobRequest;
+import pa1.ImageProcessingServer;
 import utils.ReadIn;
 
 public class JavaClient {
+    private static final String PROJ_PATH = System.getenv("PROJ_PATH");
+    private static final String CONFIG_FILE_PATH = PROJ_PATH + "config/machine.txt";
+
     public static void main(String [] args) {
         
-        String config = System.getenv("PROJ_PATH") + "/machine.txt";
-
         ReadIn r = new ReadIn();
-
-        String serverA = r.getServer(config); // server address
-        
-        String dirPath = System.getenv("PROJ_PATH") + "/" + r.getData(config); // getting path to data dir
+        String serverA = r.getServer(CONFIG_FILE_PATH); // server address
+        String dirPath = PROJ_PATH + "/" + r.getData(CONFIG_FILE_PATH); // getting path to data dir
 
         // Creating a job to send to server
         JobRequest cJob = new JobRequest(dirPath);
@@ -29,15 +31,14 @@ public class JavaClient {
         try {
             TTransport transport;
 
-            int serverPortNum = r.getServerPort(config);
-            System.out.println("Port num: " + serverPortNum + "\n");
+            int serverPortNum = r.getServerPort(CONFIG_FILE_PATH);
 
             if (serverPortNum == -1) {
                 System.out.println("Unable to get port num from machine.txt");
                 System.exit(-1);
             }
 
-            transport = new TSocket(serverA, r.getServerPort(config)); 
+            transport = new TSocket(serverA, r.getServerPort(CONFIG_FILE_PATH)); 
             transport.open();
 
             TProtocol protocol = new  TBinaryProtocol(transport);
